b98ec856d3b9019d8ac5646d8fb44894581fd887
H0R15H0 <shohori13@gmail.com>
2023-07-29 16:15:48 +0900

    use golang-parser in extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,37 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+import {GoParser} from "./golang-parser/parser/golang";
+
+const txt = `
+type user[
+  T any,
+] struct { // hoge
+  name string "Name"; // hoge
+  // b hoge.Value \`hgoefae\`
+  email *struct{ hoge string } "int"
+  age, count (int32) 
+  cType pack.Sc[T]
+  posts [100]post
+  books []int
+  comment map[string]int
+  ch <-chan <-chan int
+  getBook func(a, b string, c int, b ...bool) (hoge int, hoge)
+  getPost func(string, int, ...bool) (string)
+  getBooks func(a, b )
+  getPosts func(...string)
+  hoge, hoge string
+  inter interface{
+    hoge(hoge string) (string)
+  }
+  // hgoe
+} // hgoe
+`;
+const parser = new GoParser();
+const res = parser.parse(txt);
+console.log(txt.trim(), '=', res);
+
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -16,7 +47,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('go-getter-setter.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from go-getter-setter!');
+		vscode.window.showInformationMessage('Hello World from go-getter-setter!' + res);
 	});
 
 	context.subscriptions.push(disposable);
