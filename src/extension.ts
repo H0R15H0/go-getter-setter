fb5733b0d89cbe9ab6bd6848a2c58abec5450a08
H0R15H0 <shohori13@gmail.com>
2023-07-30 17:44:47 +0900

    User can select what field to generate

--- src/extension.ts
+++ src/extension.ts
@@ -6,6 +6,7 @@ import {GoParser} from "./golang-parser/golang";
 import * as types from "./golang-parser/types";
 import {dedent} from 'ts-dedent';
 import {SELECT_BTN_GETTER, SELECT_BTN_SETTER} from './constants';
+import { resolve } from 'path';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -19,6 +20,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('go-getter-setter.generate', () => {
+		const parser = new GoParser();
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		const editor = vscode.window.activeTextEditor;
@@ -26,7 +28,6 @@ export function activate(context: vscode.ExtensionContext) {
 			return;
 		}
     const selection = editor.selection;
-		const parser = new GoParser();
 		const text = editor.document.getText(selection);
 
 		let struct:types.Struct;
@@ -37,40 +38,15 @@ export function activate(context: vscode.ExtensionContext) {
 			return;
 		}
 
-		vscode.window.showQuickPick([SELECT_BTN_GETTER, SELECT_BTN_SETTER], { canPickMany: true, placeHolder: "select what you want to generate" }).then(items => {
-			if (items === undefined) {return;}
+		selectGenerateTargets(struct).then(targets => {
 			editor.edit((editBuilder)=> {
 				let currentLocation = new vscode.Position(selection.end.line, 0);
 				if (!text.endsWith('\n')) {
 					currentLocation = new vscode.Position(selection.end.line + 1, 0);
 				}
 				insert(editBuilder, currentLocation, '');
-				
-				for (const item of items) {
-					// Getter
-					if (item === SELECT_BTN_GETTER) {
-						for (const [i, field] of struct.fields.entries()) {
-							insert(editBuilder, currentLocation, dedent(`
-							func (${struct.name[0].toLowerCase()} *${struct.name}) ${field.name[0].toUpperCase() + field.name.slice(1)}() ${field.type} {
-								return ${struct.name[0].toLowerCase()}.${field.name}
-							}
-							`), i === struct.fields.length - 1 ? 1 : 2);
-						}
-						if (items.length === 2) {
-							insert(editBuilder, currentLocation, '');
-						}
-					}
-					// Setter
-					if (item === SELECT_BTN_SETTER) {
-						for (const [i, field] of struct.fields.entries()) {
-							insert(editBuilder, currentLocation, dedent(`
-							func (${struct.name[0].toLowerCase()} *${struct.name}) Set${field.name[0].toUpperCase() + field.name.slice(1)}(${field.name} ${field.type}) {
-								${struct.name[0].toLowerCase()}.${field.name} = ${field.name}
-							}
-							`), i === struct.fields.length - 1 ? 1 : 2);
-						}
-					}
-				}
+
+				generate(editBuilder, currentLocation, struct, targets);
 			});
 		});
 	});
@@ -83,4 +59,54 @@ export function deactivate() {}
 
 function insert(editBuilder: vscode.TextEditorEdit, location: vscode.Position, text: string, brCount: number = 1): void{
 	editBuilder.insert(location, text + '\n'.repeat(brCount));
+}
+
+type GenerateTargets = {
+	methods: string[],
+	fields: GenerateField[],
+};
+
+type GenerateField = {
+	name: string,
+	type: string,
+};
+
+async function selectGenerateTargets(struct: types.Struct): Promise<GenerateTargets> {
+	const fieldName2Field = Object.fromEntries(struct.fields.map(f => [f.name, f]));
+
+	const methods = await vscode.window.showQuickPick([SELECT_BTN_GETTER, SELECT_BTN_SETTER], { canPickMany: true, placeHolder: "select what you want to generate" })
+	const fields = await vscode.window.showQuickPick<vscode.QuickPickItem>(struct.fields.map(f => ({label: f.name, description: f.type})), { canPickMany: true, placeHolder: "select what you want to generate" });
+	if (methods === undefined || fields === undefined) {
+		return <GenerateTargets>{};
+	};
+	const generateFields = fields.map((field) => (<GenerateField>{name: field.label, type: fieldName2Field[field.label].type}));
+	return <GenerateTargets>{methods: methods, fields: generateFields};
+}
+
+function generate(editBuilder: vscode.TextEditorEdit, currentLocation: vscode.Position, struct: types.Struct, targets: GenerateTargets) {
+		for (const item of targets.methods) {
+			// Getter
+			if (item === SELECT_BTN_GETTER) {
+				for (const [i, field] of targets.fields.entries()) {
+					insert(editBuilder, currentLocation, dedent(`
+					func (${struct.name[0].toLowerCase()} *${struct.name}) ${field.name[0].toUpperCase() + field.name.slice(1)}() ${field.type} {
+						return ${struct.name[0].toLowerCase()}.${field.name}
+					}
+					`), i === targets.fields.length - 1 ? 1 : 2);
+				}
+				if (targets.methods.length === 2) {
+					insert(editBuilder, currentLocation, '');
+				}
+			}
+			// Setter
+			if (item === SELECT_BTN_SETTER) {
+				for (const [i, field] of targets.fields.entries()) {
+					insert(editBuilder, currentLocation, dedent(`
+					func (${struct.name[0].toLowerCase()} *${struct.name}) Set${field.name[0].toUpperCase() + field.name.slice(1)}(${field.name} ${field.type}) {
+						${struct.name[0].toLowerCase()}.${field.name} = ${field.name}
+					}
+					`), i === targets.fields.length - 1 ? 1 : 2);
+				}
+			}
+		}
 }
